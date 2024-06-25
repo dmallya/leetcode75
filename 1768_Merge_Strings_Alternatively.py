f43ff0d4c7545e37530a6ed02a9862fcd99a5470
s3842605 <s3842605@student.rmit.edu.au>
2024-06-25 22:05:47 +1000

    1768-attempt2

--- 1768_Merge_Strings_Alternatively.py
+++ 1768_Merge_Strings_Alternatively.py
@@ -1,15 +1,18 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        merged = []
+        word = str()
         if len(word1) > len(word2):
             for letter1, letter2 in zip(word1[: len(word2)], word2):
-                merged.extend([letter1, letter2])
-            merged.extend(word1[len(word2) :])
+                word += letter1
+                word += letter2
+            word += word1[len(word2) :]
         elif len(word1) < len(word2):
             for letter1, letter2 in zip(word1, word2[: len(word1)]):
-                merged.extend([letter1, letter2])
-            merged.extend(word2[len(word1) :])
+                word += letter1
+                word += letter2
+            word += word2[len(word1) :]
         else:
             for letter1, letter2 in zip(word1, word2):
-                merged.extend([letter1, letter2])
-        return "".join(merged)
+                word += letter1
+                word += letter2
+        return word
