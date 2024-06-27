c313fb07d80af33c4d77a531ba67f08c32c4942b
dmallya <mallya.dushyant@gmail.com>
2024-06-27 19:04:01 +1000

    151_optimal

--- Day 2/151_Reverse_Words_of_a_String.py
+++ Day 2/151_Reverse_Words_of_a_String.py
@@ -1,10 +1,4 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-        word_list = s.strip().split(" ")
-        clean_list = []
-        for word in word_list:
-            if len(word) > 0:
-                word.strip()
-                clean_list.append(word)
-                print(len(word))
-        return " ".join(clean_list[::-1])
+        arr = s.split()
+        return " ".join(arr[::-1])
