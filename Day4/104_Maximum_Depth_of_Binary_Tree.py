136d5a48bb8290c51876af1eac76719d3556494a
dmallya <mallya.dushyant@gmail.com>
2024-06-30 12:53:31 +1000

    104_attempt2

--- Day4/104_Maximum_Depth_of_Binary_Tree.py
+++ Day4/104_Maximum_Depth_of_Binary_Tree.py
@@ -11,6 +11,14 @@ class TreeNode:
 
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        if not root:
-            return 0
-        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+        stack, res = [], 0
+        if root:
+            stack.append((root, 1))
+        while stack:
+            node, depth = stack.pop()
+            res = max(res, depth)
+            if node.left:
+                stack.append((node.left, depth + 1))
+            if node.right:
+                stack.append((node.right, depth + 1))
+        return res
